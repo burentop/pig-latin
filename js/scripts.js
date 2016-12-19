b3d48e8e539fb78ec4b737560e5c41a266ba1165
Brent Perry <burentoperii@gmail.com>
2016-12-19 13:47:29 -0600

    move first consonant to end and add 'ay'.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,20 @@
+var vowels = ['a', 'e', 'i', 'o', 'u'];
+
+var isVowel = function(letter) {
+  vowels.forEach(function(vowel) {
+    if (vowel === letter) {
+      return true;
+    }
+  });
+  return false;
+}
+
 var translate = function(english) {
   var first = english[0];
   if (parseInt(first)) {
     return english;
+  } else if (!isVowel(first)) {
+    return english.slice(1) + first + "ay";
   } else {
     return english + "ay";
   }
